53efe2a9cb4b85ad0fe55eaf0b841bd92f7c2737
vidiyalarasan <vidiyalarasans@gmail.com>
2026-01-31 23:28:20 +0530

    removed Unneccessary files

--- src/main/java/com/example/studentapi/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentapi.repository;
-
-import com.example.studentapi.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
